25b3add43f7767590d08c8e7b336044aa8011e1a
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-04-27 19:02:51 -0700

    only die horizontally

--- robot.js
+++ robot.js
@@ -356,6 +356,7 @@ var Robot = function(baseAttrs, startX, destX, destY, asteroid) {
     // This is called when a robot's energy reaches
     // 0 from the handleMove function.
     var handleDeath = function() {
+        _this.animation.rotation = 0;
         _this.animation.gotoAndPlay('explode');
         _this.healthbar.visible = false;
         _this.capacitybar.visible = false;
