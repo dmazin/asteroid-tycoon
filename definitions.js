52c97e5d9c3c2e5062631d44fb392bff30cb3643
Jordan Arnesen <jordan101112@gmail.com>
2014-04-28 01:28:32 -0700

    second ast comp

--- definitions.js
+++ definitions.js
@@ -395,24 +395,40 @@ var asteroids = {
     'artifact': {pTop: 0.4, pBottom: 0.4, minDepth: .3},
     'paydirtium':{pTop: 0.1, pBottom: 1, minDepth: 0},
     'cheddarium':{pTop: 0.4, pBottom: 1, minDepth: 0.6},
-    'fatlootium': {pTop: 0.4, pBottom: 0.4, minDepth: 0.85},
+    'fatlootium': {pTop: 0.3, pBottom: 0.3, minDepth: 0.85},
     // 'affluentium': {pTop: 0, pBottom: 0, minDepth: 0},
     // 'cashmonium': {pTop: 0, pBottom: 0, minDepth: 0},
 
         // pain and death
         // 'lava': {pTop: 0, pBottom: 0, minDepth: 0}
     }, 1.0),
+
     "Cheddaroid": new Asteroid("Cheddaroid", {
-        'dirtite': {pTop: 1, pBottom: 1, minDepth: 0},
-        'dregsite': {pTop: 0.2, pBottom: 0.4, minDepth: 1},
-        'rubbishite': {pTop: 0.2, pBottom: 0.2, minDepth: 1},
-        'junkite': {pTop: 0.2, pBottom: 0.2, minDepth: 1},
-        'scrapite': {pTop: 0.2, pBottom: 0.2, minDepth: 1},
-        'crapite': {pTop: 0.1, pBottom: 0.2, minDepth: 1},
-        'paydirtium': {pTop: 0.1, pBottom: 0.05, minDepth: 1},
-        'cheddarium': {pTop: 0.1, pBottom: 0.5, minDepth: 1},
-        'fatlootium': {pTop: 0.01, pBottom: 0.05, minDepth: 5}
+    //dirt and rocks
+    'dirtite': {pTop: 1, pBottom: .5, minDepth: 0},
+    'dregsite': {pTop: 1, pBottom: 1, minDepth: 0},
+    'rubbishite': {pTop: .4, pBottom: .6, minDepth: 0.5},
+    'junkite': {pTop: 0.3, pBottom: 0.4, minDepth: 0.7},
+    // 'scrapite': {pTop: 0, pBottom: 0, minDepth: 0},
+    // 'crapite': {pTop: 0, pBottom: 0, minDepth: 0},
+    // 'densite': {pTop: 0, pBottom: 0, minDepth: 0},
+    // 'rigidite': {pTop: 0, pBottom: 0, minDepth: 0},
+    // 'toughite': {pTop: 0, pBottom: 0, minDepth: 0},
+    // 'unyieldite': {pTop: 0, pBottom: 0, minDepth: 0},
+    'imperviousite': {pTop: 1, pBottom: 0,.7 minDepth: 0.9},
+
+    // minerals and goodies
+    'artifact': {pTop: 0.4, pBottom: 0.4, minDepth: .3},
+    'paydirtium':{pTop: 1, pBottom: .6, minDepth: 0},
+    'cheddarium':{pTop: 0.8, pBottom: 1, minDepth: 0},
+    'fatlootium': {pTop: 0.3, pBottom: 0.5, minDepth: 0.6},
+    // 'affluentium': {pTop: 0, pBottom: 0, minDepth: 0},
+    // 'cashmonium': {pTop: 0, pBottom: 0, minDepth: 0},
+
+    // pain and death
+    'lava': {pTop: 0, pBottom: 0, minDepth: 0}
     }, 1.0),
+
     "Fatlooteroid": new Asteroid("Fatlooteroid", {
         'dirtite': {pTop: 1, pBottom: 1, minDepth: 0},
         'dregsite': {pTop: 0.2, pBottom: 0.4, minDepth: 1},
