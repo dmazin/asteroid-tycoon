647d4468af68efc14648645f40f0be5978046f17
Dmitry Mazin <dmazin@gmail.com>
2014-04-27 17:29:18 -0700

    More display fuckery

--- state.js
+++ state.js
@@ -15,6 +15,9 @@ var playerState = (function() {
 
     state = {};
 
+    // Set initial displays
+    $('.money-stats .amount').text('$' + parseInt(resourceAmounts['money']));
+
     state.getRobotLevel = function(robotType) {
         return robotLevels[robotType];
     };
@@ -35,7 +38,7 @@ var playerState = (function() {
         resourceAmounts[resource] = resourceAmounts[resource] || 0;
         resourceAmounts[resource] += amount;
 
-        $('.money-stats .amount').text('$ ' + parseInt(resourceAmounts['money']));
+        $('.money-stats .amount').text('$' + parseInt(resourceAmounts['money']));
         $('.notification.money .amount').text(parseInt(resourceAmounts['money']));
 
         if (resource === 'money' || resources[resource].harvestable === false) {
